320d2d6b7c0993f498a2331aee57acc25cdbfb2b
Mark.K <a4756830@gmail.com>
2022-06-21 15:40:53 +0800

    Fix Deploy issue - 2

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
-  publicPath: "/",
+  publicPath: "/cherri-exam/",
   pluginOptions: {
     i18n: {
       locale: "tw",
