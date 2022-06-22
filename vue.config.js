68f2036d220319f5132aa2068e0dd1cc6987529e
Mark.K <a4756830@gmail.com>
2022-06-22 10:06:42 +0800

    Fix local publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
-  publicPath: "/cherri-exam/",
+  publicPath: process.env.NODE_ENV === "production" ? "/cherri-exam/" : "/",
   pluginOptions: {
     i18n: {
       locale: "tw",
